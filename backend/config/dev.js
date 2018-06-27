94f50c5825506c03ebfac0ea239cef0a5d2d140d
kartikvips <kartikvips@gmail.com>
2018-06-26 20:13:44 -0700

    add user auth for facebook and google

---
+++ backend/config/dev.js
@@ -0,0 +1,8 @@
+//dev.js don't commit this
+
+module.exports = {
+    googleClientID: '649355888268-lb2k7coue8grd3gulas3e8a3uedg1ksu.apps.googleusercontent.com',
+    googleClientSecret: 'oxcGacSnCw8ViGq_OoaRWd-q',
+    mongoURI: 'mongodb://kartik:password1@ds217351.mlab.com:17351/flipt',
+    cookieKey: 'jahsdfhasdjfhasljkdhfiouhfjdhfluadshfjl'
+};
---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,38 @@
+const passport = require('passport');
+
+
+module.exports = (app) => {
+    app.get('/auth/google', passport.authenticate('google', {
+        scope: ['profile', 'email']
+    }));
+
+    // app.get('/auth/google/callback', passport.authenticate('google'));
+
+    app.get(
+        '/auth/google/callback',
+        passport.authenticate('google', {
+            failureRedirect: '/auth/google'
+        }),
+        (req, res) =>
+        res.redirect('flipt://login?user=' + JSON.stringify(req.user))
+    );
+
+    // app.get(
+    //     '/auth/google/callback',
+    //     passport.authenticate('google', {
+    //         failureRedirect: '/auth/google'
+    //     }),
+    //     (req, res) =>
+    //     res.send(req.user)
+    // );
+
+    app.get('/api/logout', (req, res) => {
+        req.logout();
+        res.send(req.user);
+    });
+
+    app.get('/api/current_user', (req, res) => {
+        res.send(req.user);
+    });
+
+};
