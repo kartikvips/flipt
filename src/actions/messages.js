b7d05c588b7b9fd4960dc4c578cb055fd07e99f7
Tiffany Tang <amyamyxtiffany@gmail.com>
2018-07-02 07:30:57 -0700

    Modify message api, actions and reducer

--- src/actions/messages.js
+++ src/actions/messages.js
@@ -1,13 +1,24 @@
 import * as MessageApiUtil from '../util/message_api_util';
 import { RECEIVE_ALL_MESSAGES } from './types';
 
-const receiveMessages = payload => ({
-  type: RECEIVE_MESSAGES,
-  payload
+const receiveAllMessages = messages => ({
+  type: RECEIVE_ALL_MESSAGES,
+  payload: messages
+})
+
+const receiveMessage = message => ({
+  type: RECEIVE_MESSAGE,
+  payload: message
 })
 
 export const fetchMessages = chatId => dispatch => (
   MessageApiUtil.fetchMessages(chatId)
-    .then(messages => dispatch(receiveMessages(messages)))
+    .then(messages => dispatch(receiveAllMessages(messages)))
     .then(err => console.log("error in fetchMessages: ", err))
+)
+
+export const createMessage = (message) => dispatch(
+  MessageApiUtil.createMessage(message)
+    .then(message => dispatch(receiveMessage(message)))
+    .then(err => console.log("error in createMessage: ", err))
 )
